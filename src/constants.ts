88384bac85ac3b4170eeacb62fea86a3a5c11d6e
Munkyfoot <wopas182@gmail.com>
2023-11-13 15:35:16 -0800

    Update constants.

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 import { NavItem } from "@/types"
 
-export const siteTitle = "Example Site"
-export const siteDescription = "Example site description"
+export const siteTitle = "Next.js Starter with Chakra UI"
+export const siteDescription =
+    "Next.js starter template with TypeScript, Chakra UI, Custom Components, and more to help you get started quickly."
 export const siteCanonical = "https://example.com"
-export const siteKeywords = "example, keywords"
-export const siteAuthor = "Example Author"
+export const siteAuthor = "Munkyfoot"
 export const siteNavItems: NavItem[] = [{ name: "Start Here", href: "/" }]
