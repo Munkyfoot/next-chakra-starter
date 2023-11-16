9fe8188a09fa4c2e47d2955cf53d92932ce85da4
Munkyfoot <wopas182@gmail.com>
2023-11-16 13:56:25 -0800

    Modify delay for SlideFadeOnView components that start below the fold.

--- src/components/SlideFadeOnView.tsx
+++ src/components/SlideFadeOnView.tsx
@@ -1,12 +1,37 @@
 import { SlideFade, SlideFadeProps } from "@chakra-ui/react"
 import { useInView } from "framer-motion"
-import { useRef } from "react"
+import { useRef, useEffect, useState } from "react"
 
 const SlideFadeOnView = (props: SlideFadeProps) => {
-    const ref = useRef(null)
+    const ref = useRef<HTMLDivElement>(null)
     const isInView = useInView(ref, { once: true })
+    const [startInView, setStartInView] = useState(false)
 
-    return <SlideFade ref={ref} in={isInView} {...props} />
+    useEffect(() => {
+        if (ref.current) {
+            const rect = ref.current.getBoundingClientRect()
+            const startsInView = rect.top >= 0 && rect.top <= window.innerHeight
+            setStartInView(startsInView)
+        }
+    }, [])
+
+    const adjustedProps = startInView
+        ? props
+        : {
+              ...props,
+              transition: {
+                  ...props.transition,
+                  enter: {
+                      ...props.transition?.enter,
+                      // @ts-ignore
+                      delay: props.transition?.enter?.duration / 2 || 0,
+                  },
+              },
+          }
+
+    console.log(adjustedProps)
+
+    return <SlideFade ref={ref} in={isInView} {...adjustedProps} />
 }
 
 export default SlideFadeOnView
